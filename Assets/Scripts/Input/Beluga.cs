1d812f1e2cebbccf9832d1d1c33c3af24f6c0c4b
alexandrehdmd <alexandrehoudemond49@gmail.com>
2026-05-14 17:06:49 +0200

    Fix mouvements belugas

--- Assets/Scripts/Input/Beluga.cs
+++ Assets/Scripts/Input/Beluga.cs
@@ -1,36 +1,38 @@
-// using System;
-// using UnityEngine;
-// using Random = UnityEngine.Random;
-//
-// public class Beluga : MonoBehaviour
-// {
-//
-//     [SerializeField] private Rigidbody2D _rigidbody2D;
-//     private void Update()
-//     {
-//         BelugaMove();
-//     }
-//
-//
-//     private BelugaState _state;
-//
-//     private float _moveSpeed = 5f;
-//
-//     public Beluga()
-//     {
-//         _state = BelugaState.Idle;
-//     }
-//
-//     private void BelugaTrajectory()
-//     {
-//         Vector3 direction = Vector3.zero;
-//         direction.x += Random.Range(-2, 2);
-//         direction.y += Random.Range(-2, 2);
-//         direction.Normalize();
-//     }
-//     private void BelugaMove()
-//     {
-//         _rigidbody2D.linearVelocity = direction * _moveSpeed;
-//
-//     }
-// }
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Beluga : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    private float _moveSpeed = 2.5f;
+    private float _waitingTimer = 0f;
+    private bool isTrapped;
+    
+    public Beluga()
+    {
+        isTrapped = false;
+    }
+    
+    private void BelugaMove()
+    {
+        Vector3 direction = Vector3.zero;
+        direction.x += Random.Range(-2, 2);
+        direction.y += Random.Range(-2, 2);
+        direction.Normalize();
+        _rigidbody2D.linearVelocity = direction * _moveSpeed;
+
+    }
+    
+    private void Update()
+    {
+        _waitingTimer += Time.deltaTime;
+        if (_waitingTimer >= Random.Range(1, 2))
+        {
+            _waitingTimer = 0f;
+            BelugaMove();
+        }
+    }
+    
+}
