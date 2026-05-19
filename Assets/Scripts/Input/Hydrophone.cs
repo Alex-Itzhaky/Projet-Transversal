1980cdc43e5397af3db04d3ae5f416f9ff79294f
alexandrehdmd <alexandrehoudemond49@gmail.com>
2026-05-19 11:57:57 +0200

    micro fix hydrophone

--- Assets/Scripts/Input/Hydrophone.cs
+++ Assets/Scripts/Input/Hydrophone.cs
@@ -7,7 +7,7 @@ public class Hydrophone : MonoBehaviour
     [SerializeField] private SpriteRenderer _spriteRenderer;
     private SpriteRenderer _belugaSprite;
     private bool _isBroken = false;
-    private Color _neutral = new Color(0,0,255);
+    private Color _neutral = new Color(255,255,255);
     private Color _belugaDetected = new Color(255,0,0);
     private Color _broken = new Color(0, 0, 0);
     
